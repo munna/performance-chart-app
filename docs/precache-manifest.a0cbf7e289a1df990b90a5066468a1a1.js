c5a56ee82e22e6f1dfe990242bb5bbe2e890f410
Munna Bhakta <munna@ferventsoft.com>
2019-10-25 12:15:32 +0530

    added doc folder to show demo

---
+++ docs/precache-manifest.a0cbf7e289a1df990b90a5066468a1a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90ad82fed0f9c91af4e6bb0d025ede8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a05b7f3ef25a2e2f659",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "51dcdf0bd7204c89a26c",
+    "url": "/static/js/2.969c12d3.chunk.js"
+  },
+  {
+    "revision": "3a05b7f3ef25a2e2f659",
+    "url": "/static/js/main.4436e144.chunk.js"
+  },
+  {
+    "revision": "71944013297a834f0f4d",
+    "url": "/static/js/runtime~main.8bbeaa21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
