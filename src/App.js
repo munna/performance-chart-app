ac64aaf56ad2d4efe71354733d97e4a5f4216f28
Munna Bhakta <munna@ferventsoft.com>
2019-10-25 11:18:32 +0530

    Demo for chart application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
+import PerformanceChart from 'performance-chart';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const data=[
+    {name: "BOTTOM", value: 20, color: "#FF0000"},
+    {name: "Neutral", value: 20, color: "#FFA500"},
+    {name: "AVERAGE", value: 20, color: "#FFFF00"},
+    {name: "No comment", value: 20, color: "#7CFC00"},
+    {name: "TOP", value: 20, color: "#008000"}
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PerformanceChart size="300" current="10" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="56" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="76" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="32" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="88" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="96" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="99" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="100" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="10" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="56" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="76" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="32" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="88" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="96" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="99" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="100" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="10" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="56" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="76" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="32" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="88" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="96" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="99" data={data}></PerformanceChart>
+      <PerformanceChart size="300" current="100" data={data}></PerformanceChart>
     </div>
   );
 }
