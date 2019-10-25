ec39b18073f601360a0c668ea6c802e7e0c03563
Munna Bhakta <munna@ferventsoft.com>
2019-10-25 12:52:12 +0530

    rebuild with name change

---
+++ docs/precache-manifest.337af0531d26ee586694fe9f7bb7bc10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbe1f5760a4dbe848107189c415675b",
+    "url": "/performance-chart-app/index.html"
+  },
+  {
+    "revision": "5b460904ffffa3e5e746",
+    "url": "/performance-chart-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "51dcdf0bd7204c89a26c",
+    "url": "/performance-chart-app/static/js/2.969c12d3.chunk.js"
+  },
+  {
+    "revision": "5b460904ffffa3e5e746",
+    "url": "/performance-chart-app/static/js/main.62e84492.chunk.js"
+  },
+  {
+    "revision": "d1e22b151a58c573aa11",
+    "url": "/performance-chart-app/static/js/runtime~main.660e4717.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/performance-chart-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/progress-chart-app/precache-manifest.110a5b03c6252e3055b85268c8578421.js"
+  "/performance-chart-app/precache-manifest.337af0531d26ee586694fe9f7bb7bc10.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/progress-chart-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/performance-chart-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
